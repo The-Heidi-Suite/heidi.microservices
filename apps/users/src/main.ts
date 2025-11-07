69269f2427bf232477a1d664710e2cf34da13dd4
Saurabh Patel <dev7@heidi-app.com>
2025-11-07 12:11:26 +0530

    fix(swagger): temporarily disable i18n options in Swagger setup
    - Commented out the i18n options in Swagger setup to address a null response issue.
    - Added a TODO note to re-enable i18n options once the issue is resolved, ensuring future maintainability.

--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -5,7 +5,7 @@ import helmet from 'helmet';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { LoggerService } from '@heidi/logger';
-import { ConfigService, getSwaggerServerUrl, getSwaggerI18nOptions } from '@heidi/config';
+import { ConfigService, getSwaggerServerUrl /*, getSwaggerI18nOptions */ } from '@heidi/config';
 import { getRmqConsumerOptions } from '@heidi/rabbitmq';
 
 async function bootstrap() {
@@ -80,12 +80,13 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
 
   // Use i18n-enabled Swagger options
-  const swaggerI18nOptions = getSwaggerI18nOptions(configService);
+  // TODO: Re-enable i18n options once the null response issue is fixed
+  // const swaggerI18nOptions = getSwaggerI18nOptions(configService);
 
   SwaggerModule.setup('docs', app, document, {
-    ...swaggerI18nOptions,
+    // ...swaggerI18nOptions,
     swaggerOptions: {
-      ...swaggerI18nOptions.swaggerOptions,
+      // ...swaggerI18nOptions.swaggerOptions,
       persistAuthorization: true,
     },
   });
